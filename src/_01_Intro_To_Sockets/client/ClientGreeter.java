557502e51bc20398079ad154ff2dc0d12ba6b0cf
monij <monij@DESKTOP-4MC4PHB.home>
2021-01-13 17:34:18 -0800

    intro to sockets(WIP)

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -8,11 +8,11 @@ public class ClientGreeter {
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
+      String ip = "192.168.1.8";
       //2. Create an integer for the server's port number
-      
+      int port = 8080;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
-    
+    try {
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
  
          //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
@@ -25,5 +25,8 @@ public class ClientGreeter {
          
          //9. Close the client's server object
 
+    } catch (Exception e) {
+		// TODO: handle exception
+	}
    }
 }
