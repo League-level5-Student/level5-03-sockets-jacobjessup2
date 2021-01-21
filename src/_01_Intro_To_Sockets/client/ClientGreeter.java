d6ecdf1f90f6503a098dba0dfe2dcd8770e4a405
monij <monij@DESKTOP-4MC4PHB.home>
2021-01-20 17:28:50 -0800

    Client/Server greeter(WIP)

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -14,18 +14,18 @@ public class ClientGreeter {
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
     try {
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
+         Socket socket = new Socket(ip, port);
          //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-         
+         DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
          //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
+         dos.writeUTF("hi");
          //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
+         DataInputStream dis = new DataInputStream(socket.getInputStream());
          //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
+         System.out.println(dis.readUTF());
          //9. Close the client's server object
-
-    } catch (Exception e) {
+         socket.close();
+    } catch (IOException e) {
 		// TODO: handle exception
 	}
    }
