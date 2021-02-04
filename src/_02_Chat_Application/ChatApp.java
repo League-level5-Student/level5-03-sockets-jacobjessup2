abb40e46b0f518054407ec101a4f75483e8d6030
monij <monij@DESKTOP-4MC4PHB.home>
2021-02-03 17:26:45 -0800

    Chat App(WIP)

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -3,6 +3,8 @@ package _02_Chat_Application;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import _00_Click_Chat.gui.ButtonClicker;
 import _00_Click_Chat.networking.Client;
@@ -13,27 +15,32 @@ import _00_Click_Chat.networking.Server;
  */
 
 public class ChatApp extends JFrame{
-JButton button = new JButton("CLICK");
-	
-	Server server;
-	Client client;
+JPanel panel = new JPanel();
+JButton button = new JButton("send");
+JTextField text = new JTextField("                        ");
+	Server2 server;
+	Client2 client;
 	
 	
 	public static void main(String[] args) {
-		new ButtonClicker();
+		new ChatApp();
 	}
 	
 	public ChatApp(){
 		
 		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!", JOptionPane.YES_NO_OPTION);
 		if(response == JOptionPane.YES_OPTION){
-			server = new Server(8080);
+			server = new Server2(8080);
 			setTitle("SERVER");
 			JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
 			button.addActionListener((e)->{
-				server.sendClick();
+				System.out.println(text.getText());
+				client.sendMessage(text.getText());
+				text.setText("                        ");
 			});
-			add(button);
+			add(panel);
+			panel.add(button);
+			panel.add(text);
 			setVisible(true);
 			setSize(400, 300);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,12 +51,15 @@ JButton button = new JButton("CLICK");
 			String ipStr = JOptionPane.showInputDialog("Enter the IP Address");
 			String prtStr = JOptionPane.showInputDialog("Enter the port number");
 			int port = Integer.parseInt(prtStr);
-			client = new Client(ipStr, port);
+			client = new Client2(ipStr, port);
 			button.addActionListener((e)->{
-				client.sendClick();
+				System.out.println(text.getText());
+				client.sendMessage(text.getText());
+				text.setText("                        ");
 			});
-			add(button);
-			setVisible(true);
+			add(panel);
+			panel.add(button);
+			panel.add(text);
 			setSize(400, 300);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			client.start();
